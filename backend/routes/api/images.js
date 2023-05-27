f3cd6542edfe1f8242cc0b1d978d20a91b6cb9af
Tristan Allaman <tristanallaman@gmail.com>
2023-05-26 17:22:32 -0700

    Added delete a spot image route

--- backend/routes/api/images.js
+++ backend/routes/api/images.js
@@ -13,11 +13,44 @@ const { requireAuth } = require("../../utils/auth");
 const { Op } = require("sequelize");
 const { handleValidationErrors } = require("../../utils/validation");
 
+//Delete a spot image
 
+router.delete("/:id", requireAuth, async (req, res) => {
+  const imageId = +req.params.id;
+  const userId = req.user.id;
 
-router.delete('/:id', requireAuth, async (req, res) => {
-    const imageId = +req.params.id;
+  try {
+    const spotImage = await Image.findOne({
+      where: {
+        id: imageId,
+        imageableType: "Spot",
+      },
+      include: {
+        model: Spot,
+        as: "spot",
+        attributes: ["ownerId"],
+      },
+    });
 
-})
+    if (!spotImage) {
+      res.status(404).json({ message: "Spot Image couldn't be found" });
+      return;
+    }
 
-module.exports = router
+    if (spotImage.spot.ownerId !== userId) {
+      res.status(403).json({
+        message: "You can not delete a spot image that does not belong to you",
+      });
+      return;
+    }
+
+    await spotImage.destroy();
+
+    res.status(200).json({ message: "Successfully deleted" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal Server Error " });
+  }
+});
+
+module.exports = router;
