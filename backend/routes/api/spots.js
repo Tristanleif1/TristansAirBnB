446d1d4f19379730378fa71ea07f84eb9cc42bab
Tristan Allaman <tristanallaman@gmail.com>
2023-05-28 13:22:55 -0700

    Added group by clause for spot.id and spotimages.id

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -244,6 +244,7 @@ router.get("/", async (req, res) => {
         required: false,
         limit: 1,
       },
+      group: ["Spot.id", "SpotImages.id"],
     });
 
     const spotsWithImage = allSpots.map((spot) => ({
