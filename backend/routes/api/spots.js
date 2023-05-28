19817321c7037a16aaf57f88bbd911e1afc795c3
Tristan Allaman <tristanallaman@gmail.com>
2023-05-28 13:14:21 -0700

    Commented get all spots back into spots router

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -232,52 +232,52 @@ router.get("/", validQueryParameters, async (req, res) => {
   }
 });
 
-//Get all spots -- needs to be worked on
+// Get all spots -- needs to be worked on
 
-// router.get("/", async (req, res) => {
-//   try {
-//     const allSpots = await Spot.findAll({
-//       include: {
-//         model: Image,
-//         as: "SpotImages",
-//         attributes: ["url"],
-//         required: false,
-//         limit: 1,
-//       },
-//     });
+router.get("/", async (req, res) => {
+  try {
+    const allSpots = await Spot.findAll({
+      include: {
+        model: Image,
+        as: "SpotImages",
+        attributes: ["url"],
+        required: false,
+        limit: 1,
+      },
+    });
 
-//     const spotsWithImage = allSpots.map((spot) => ({
-//       id: spot.id,
-//       ownerId: spot.ownerId,
-//       address: spot.address,
-//       city: spot.city,
-//       state: spot.state,
-//       country: spot.country,
-//       lat: spot.lat,
-//       lng: spot.lng,
-//       name: spot.name,
-//       description: spot.description,
-//       price: spot.price,
-//       createdAt: spot.createdAt,
-//       updatedAt: spot.updatedAt,
-//       avgRating: spot.avgRating,
-//       previewImage: spot.SpotImages.length ? spot.SpotImages[0].url : null,
-//     }));
+    const spotsWithImage = allSpots.map((spot) => ({
+      id: spot.id,
+      ownerId: spot.ownerId,
+      address: spot.address,
+      city: spot.city,
+      state: spot.state,
+      country: spot.country,
+      lat: spot.lat,
+      lng: spot.lng,
+      name: spot.name,
+      description: spot.description,
+      price: spot.price,
+      createdAt: spot.createdAt,
+      updatedAt: spot.updatedAt,
+      avgRating: spot.avgRating,
+      previewImage: spot.SpotImages.length ? spot.SpotImages[0].url : null,
+    }));
 
-//     const properResponse = {
-//       Spots: spotsWithImage,
-//     };
-//     //   res.json(allSpots);
-//     // const properResponse = {
-//     //   Spots: allSpots,
-//     // };
+    const properResponse = {
+      Spots: spotsWithImage,
+    };
+    //   res.json(allSpots);
+    // const properResponse = {
+    //   Spots: allSpots,
+    // };
 
-//     res.status(200).json(properResponse);
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).json({ error: "Spots could not be found" });
-//   }
-// });
+    res.status(200).json(properResponse);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Spots could not be found" });
+  }
+});
 //GET the current user's spots
 router.get("/mySpots", requireAuth, async (req, res) => {
   const { user } = req;
