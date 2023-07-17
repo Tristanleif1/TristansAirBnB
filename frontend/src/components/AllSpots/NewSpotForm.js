7a80af1953d7dfa7b6a02fd423020f2a6607e382
Tristan Allaman <tristanallaman@gmail.com>
2023-07-17 12:51:16 -0700

    Adjusted descripiton error response

--- frontend/src/components/AllSpots/NewSpotForm.js
+++ frontend/src/components/AllSpots/NewSpotForm.js
@@ -35,7 +35,7 @@ const NewSpotForm = () => {
       newErrors.name = "Name is required.";
     }
     if (!description) {
-      newErrors.description = "Description is required.";
+      newErrors.description = "Description needs 30 or more characters.";
     } else if (description.length < 30) {
       newErrors.description = "Description needs 30 or more characters.";
     }
@@ -107,7 +107,10 @@ const NewSpotForm = () => {
 
       <form className="spot-form" onSubmit={handleSubmit}>
         <h2>Where's your place located?</h2>
-        <p>Guests will only get your exact address once they booked a reservation.</p>
+        <p>
+          Guests will only get your exact address once they booked a
+          reservation.
+        </p>
         <input
           id="country"
           type="text"
@@ -142,7 +145,10 @@ const NewSpotForm = () => {
         {renderError("state")}
 
         <h2>Describe your place to guests</h2>
-        <p>Mention the best features of your space, any special amenities like fast wifi or parking, and what you love about the neighborhood.</p>
+        <p>
+          Mention the best features of your space, any special amenities like
+          fast wifi or parking, and what you love about the neighborhood.
+        </p>
         <textarea
           placeholder="Please write at least 30 characters"
           value={description}
@@ -151,7 +157,10 @@ const NewSpotForm = () => {
         {renderError("description")}
 
         <h2>Create a title for your spot</h2>
-        <p>Catch guests' attention with a spot title that highlights what makes your place special.</p>
+        <p>
+          Catch guests' attention with a spot title that highlights what makes
+          your place special.
+        </p>
         <input
           type="text"
           placeholder="Name of your spot"
@@ -161,7 +170,10 @@ const NewSpotForm = () => {
         {renderError("name")}
 
         <h2>Set a base price for your spot</h2>
-        <p>Competitive pricing can help your listing stand out and rank higher in search results.</p>
+        <p>
+          Competitive pricing can help your listing stand out and rank higher in
+          search results.
+        </p>
         <input
           type="number"
           placeholder="Price per night (USD)"
